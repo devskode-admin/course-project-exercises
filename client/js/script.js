384c30b0dc4429df2a9de22b99d4fb43e883f625
LucianoFraileLopez22 <luciano.lopez@devskode.com>
2023-12-13 19:00:20 -0300

    fe-exercises-01: finish exercises 1

--- client/js/script.js
+++ client/js/script.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Al cargar la página, realiza la solicitud a la API
-    fetch('http://localhost:4000/professionals')
+    fetch('http://localhost:4000/technologies')
         .then(response => response.json())
         .then(data => displayTechnologies(data))
         .catch(error => console.error('Error fetching data:', error));
 });
 
 function displayTechnologies(technologies) {
-    const technologyList = document.getElementById('technologyList');
+    const technologyListBody = document.getElementById('technologyList');
+    technologyListBody.innerHTML = "";
 
-    // Recorre la lista de tecnologías y crea elementos de lista
-    technologies.forEach(tech => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `ID: ${tech.id}, Nombre: ${tech.nombre}`;
-        technologyList.appendChild(listItem);
+    technologies.data.forEach(tech => {
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${tech._id}</td><td>${tech.name}</td>`;
+        technologyListBody.appendChild(row);
     });
 }
