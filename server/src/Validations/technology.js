6e22753d6e968ec02b7047e92eba8ef1fc7d1b48
Rodri <rodrigo.fontan@devskode.com>
2023-12-20 12:28:11 -0300

    feat: Backend exercise 2 example

--- server/src/Validations/technology.js
+++ server/src/Validations/technology.js
@@ -2,25 +2,17 @@ import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
   const technologyCreate = Joi.object({
-    name: Joi.string()
-      .min(3)
-      .max(20)
-      .required()
-      .messages({
-        'string.base': 'Name must contain only letters',
-        'string.empty': 'Name is required',
-        'string.min': 'Name must have at least 3 chars',
-        'string.max': 'Name cannot be more than 20 chars',
-        'any.required': 'Name is required',
-      }),
-    development_side: Joi.string()
-      .valid('Frontend', 'Backend', 'Fullstack')
-      .required()
-      .messages({
-        'any.only':
-          'Development side must be one of this: Frontend, Backend, Fullstack',
-        'any.required': 'Development is required',
-      }),
+    name: Joi.string().min(3).max(20).required().messages({
+      'string.base': 'Name must contain a String',
+      'string.empty': 'Name could not be empty', // The property is empty
+      'string.min': 'Name must have at least 3 chars',
+      'string.max': 'Name cannot be more than 20 chars',
+      'any.required': 'Name is required', // The property was not found in the body
+    }),
+    development_side: Joi.string().valid('Frontend', 'Backend', 'Fullstack').required().messages({
+      'any.only': 'Development side must be one of this: Frontend, Backend, Fullstack',
+      'any.required': 'Development is required',
+    }),
   });
 
   const validation = technologyCreate.validate(req.body);
@@ -39,7 +31,7 @@ const validateCreation = (req, res, next) => {
 const validateUpdate = (req, res, next) => {
   const technologyUpdate = Joi.object({
     name: Joi.string().min(3).max(10).messages({
-      'string.base': 'Name must contain only letters',
+      'string.base': 'Name must contain a String',
       'string.min': 'Name must have at least 3 chars',
       'string.max': 'Name cannot be more than 20 chars',
     }),
